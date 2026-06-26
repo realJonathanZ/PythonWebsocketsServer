fbfd6c5021c6f63959e9fb76a684b3da5504d17a
JonathanZ <zez451@usask.ca>
2026-06-25 20:21:56 -0600

    (working example) local clients and main localhost server, with seperated functionality on functions defined invovled in server handler. Currently: One handler func and one broadcast func

--- server/server.py
+++ server/server.py
@@ -1,24 +1,64 @@
 import asyncio
 import websockets
+import json
+from websockets.asyncio.server import ServerConnection
 
-connected_clients = set() # storing all active clients
+connected_clients: set[ServerConnection] = set() # storing all active clients
 
-async def handler(websocket):
+
+async def broadcast(sender: ServerConnection, message: str):
+    """
+    let the sender send one message and broadcast it out except to the sender itself.
+    """
+
+    disconnected_clients: set[ServerConnection] = set() # store disconnected clients to remove them later
+
+    for client in list(connected_clients):
+        # sender do not send message to itself..
+        if client != sender:
+            # send the parsed json str message out
+            try:
+                await client.send(message)
+
+            except websockets.ConnectionClosed:
+                # register this websocket connection as disconnected to the set.
+                disconnected_clients.add(client)
+
+    # clean up dead connections..
+    for client in disconnected_clients:
+        connected_clients.discard(client) # discard(): more-safe remove()
+
+
+async def handler(websocket: ServerConnection):
+    """
+    for each client connection that's made to this server,
+    this handler function will be called to handle the connection.
+    """
+
     # each client will get their own instance of this handler function.
     print("A client just connected")
-
     connected_clients.add(websocket) # add the new client to the set
+    print(f"connected total clients count: {len(connected_clients)}")
     
     try:
-        # this loop ends only when the WebSocket connection closes or an error happens.
-        async for message in websocket: # listen for messages from the client
-        # async for: keeps waiting for the next item in the stream of the presistent WebSocket connection.
-            print("Received message:", message)
+        # this loop ends only when the WebSocket connection closes or an error happens. (for a particular ServerConncetion)
+        async for message in websocket: # listen for messages from this client
+            # parse json and print its contents (Only for logging and debugging purpose)
+            try:
+                packet = json.loads(message)  # Note: debugged point: check difference with .load() and .loads()
 
-            # broadcast the message to all other connected clients
-            for client in connected_clients:
-                if client != websocket: # don't send the message back to the sender
-                    await client.send(message) # await the message is actually sent, before move on
+                print(
+                    f"received one message from one client, inspected as below:\n"
+                    f"type={packet.get('type', 'unknown')}\n"
+                    f"sender={packet.get('data', {}).get('sender', 'unknown')}"  # define quick getter mthod TODO
+                    f"message={packet.get('data', {}).get('message', 'unknown')}\n"
+                )
+
+            except json.JSONDecodeError:
+                print("main server received invalid json (or problem when loading):", message)
+
+            await broadcast(websocket, message) # broadcast the message to all other clients
+
 
     except websockets.ConnectionClosed:
         # normal disconnect (no error) # happens when ctrl-c or crash
@@ -29,18 +69,22 @@ async def handler(websocket):
 
     # when client disconnects...
     finally: # remove registered clent even if error occurs
-        connected_clients.remove(websocket)
+        connected_clients.discard(websocket) # discard(): more-safe remove() # removing it twice for safety..
         print("A client just disconnected")
+        print(f"remaining connected total clients count: {len(connected_clients)}")
+
+
 
 async def main():
     server = await websockets.serve(
         handler,        # what runs when server started? # the handler function to handle incoming connections
-        "localhost",    # who can connect? now: only this pc # host to listen on
-        8765            # port number? # port to listen on
+        host = "localhost",    # who can connect? now: only this pc # host that listens on
+        port = 8765            # port number? # port to listen on
     )
 
     print("WebSocket server running on ws://localhost:8765") # ws:// is the WebSocket protocol
 
+    # await: process one message at a time for this ServerConnection instance(i.e. one client)
     await server.wait_closed() # don't exit, keep the server alive forever
 
 
